bb07a7a7255d13317302c965abea40420b0e4045
= <=>
2020-07-10 13:07:51 +0700

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { DangkiComponent } from './dangki/dangki.component';
+import { DangnhapComponent } from './dangnhap/dangnhap.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule, HttpClientModule,
+  RouterModule.forRoot([
+    {path: 'dangki', component:DangkiComponent},
+    {path: 'dangnhap', component:DangnhapComponent}
+  ]) ],
+  declarations: [ AppComponent, HelloComponent, DangkiComponent, DangnhapComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
