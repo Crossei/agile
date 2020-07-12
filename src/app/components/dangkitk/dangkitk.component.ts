4036804b87788ad438e2b59d896a8df9d0e1de85
Hieupmph08087 <Hieupmph08087@fpt.edu.vn>
2020-07-12 19:16:38 +0700

    suadoiti

---
+++ src/app/components/dangkitk/dangkitk.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dangkitk',
+  templateUrl: './dangkitk.component.html',
+  styleUrls: ['./dangkitk.component.css']
+})
+export class DangkitkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
